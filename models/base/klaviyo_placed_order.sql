406e2be9caf9a4f70e81b655195a3bbf016f5d5d
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-04 15:40:37 +0100

    Update klaviyo_placed_order.sql

--- models/base/klaviyo_placed_order.sql
+++ models/base/klaviyo_placed_order.sql
@@ -1,16 +1,24 @@
 {%- set klav_schema_name, klav_schema_name = 'supermetrics_raw', 'klav_placed_orders' -%}
 
 {%- set sho_schema_name, sho_table_name = 'shopify_base', 'shopify_orders' -%}
+{%- set sho_table_exists = check_source_exists(sho_schema_name, sho_table_name) %}
 
 WITH klaviyo_data AS (
-  SELECT date, campaign_id, purchase_id::varchar AS order_id
+  SELECT 
+    date, 
+    campaign_id, 
+    purchase_id::varchar AS order_id,
+    sum(coalesce(shopify_placed_order,0)) as klav_orders,
+    sum(coalesce(shopify_placed_order_value,0)) as klav_revenue
   FROM {{ source(klav_schema_name,klav_schema_name) }}
   WHERE purchase_id != ''
   AND campaign_id != ''
   AND shopify_placed_order > 0
+  GROUP BY 1,2,3
   ORDER BY date ASC
 )
-  
+
+{%- if sho_table_exists %}
 , shopify_data AS (
   SELECT order_id::varchar, 
   datediff(day,customer_acquisition_date,date) AS delay,
@@ -40,3 +48,21 @@ WITH klaviyo_data AS (
 SELECT *,
     campaign_id||date as unique_key
 FROM placed_order_data
+
+{%- else %}
+
+, placed_order_data AS (
+  SELECT
+  date, 
+  campaign_id,
+  sum(klav_orders) as total_orders,
+  sum(klav_revenue) as total_revenue
+  FROM klaviyo_data 
+  WHERE campaign_id != ''
+  GROUP BY 1,2
+)
+
+SELECT *,
+    campaign_id||date as unique_key
+FROM placed_order_data
+{%- endif %}
