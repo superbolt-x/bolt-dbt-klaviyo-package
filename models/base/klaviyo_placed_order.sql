22322a39dbdc1f833b302ac75d95ebca3a99cc2d
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-04 15:14:14 +0100

    Update klaviyo_placed_order.sql

--- models/base/klaviyo_placed_order.sql
+++ models/base/klaviyo_placed_order.sql
@@ -2,31 +2,41 @@
 
 {%- set sho_schema_name, sho_table_name = 'shopify_base', 'shopify_orders' -%}
 
-with klaviyo_data as (select date, campaign_id, purchase_id::varchar as order_id
-from {{ source(klav_schema_name,klav_schema_name) }}
-where purchase_id != ''
-and campaign_id != ''
-and shopify_placed_order > 0
-order by date asc)
+WITH klaviyo_data AS (
+  SELECT date, campaign_id, purchase_id::varchar AS order_id
+  FROM {{ source(klav_schema_name,klav_schema_name) }}
+  WHERE purchase_id != ''
+  AND campaign_id != ''
+  AND shopify_placed_order > 0
+  ORDER BY date ASC
+)
   
-, shopify_data as (select order_id::varchar, 
-datediff(day,customer_acquisition_date,date) as delay,
-case when customer_order_index = 1 then 1 else 0 end as first_orders,
-case when customer_order_index > 1 then 1 else 0 end as repeat_orders,
-case when customer_order_index = 1 then total_revenue else 0 end as first_revenue,
-case when customer_order_index > 1 then total_revenue else 0 end as repeat_revenue
-from {{ source(shopify_base,shopify_orders) }}
+, shopify_data AS (
+  SELECT order_id::varchar, 
+  datediff(day,customer_acquisition_date,date) AS delay,
+  CASE WHEN customer_order_index = 1 THEN 1 ELSE 0 END AS first_orders,
+  CASE WHEN customer_order_index > 1 THEN 1 ELSE 0 END AS repeat_orders,
+  CASE WHEN customer_order_index = 1 THEN total_revenue ELSE 0 END AS first_revenue,
+  CASE WHEN customer_order_index > 1 THEN total_revenue ELSE 0 END AS repeat_revenue
+  FROM {{ source(shopify_base,shopify_orders) }}
 )
 
-select
-date, 
-campaign_id,
-sum(first_orders+repeat_orders) as total_orders,
-sum(first_orders) as first_orders,
-sum(repeat_orders) as repeat_orders,
-sum(first_revenue+repeat_revenue) as total_revenue,
-sum(first_revenue) as first_orders_revenue,
-sum(repeat_revenue) as repeat_orders_revenue
-from klaviyo_data left join shopify_data USING(order_id)
-where campaign_id != ''
-group by 1,2
+, placed_order_data AS (
+  SELECT
+  date, 
+  campaign_id,
+  sum(first_orders+repeat_orders) as total_orders,
+  sum(first_orders) as first_orders,
+  sum(repeat_orders) as repeat_orders,
+  sum(first_revenue+repeat_revenue) as total_revenue,
+  sum(first_revenue) as first_orders_revenue,
+  sum(repeat_revenue) as repeat_orders_revenue
+  FROM klaviyo_data 
+  LEFT JOIN shopify_data USING(order_id)
+  WHERE campaign_id != ''
+  GROUP BY 1,2
+)
+
+SELECT *,
+    campaign_id||date as unique_key
+FROM placed_order_data
