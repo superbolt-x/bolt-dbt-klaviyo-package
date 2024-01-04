7505ae84297293e6301067c4629e58da566ec034
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-04 15:53:12 +0100

    Update klaviyo_performance_by_flow.sql

--- models/reporting/klaviyo_performance_by_flow.sql
+++ models/reporting/klaviyo_performance_by_flow.sql
@@ -1,142 +1,50 @@
 {{ config (
     alias = target.database + '_klaviyo_performance_by_flow'
 )}}
-{%- set shopify_schema_name, shopify_table_name = 'shopify', 'shopify_orders' -%}
-{%- set shopify_table_exists = check_source_exists(shopify_schema_name, shopify_table_name) %}
+
+{%- set sho_table_exists = check_source_exists(shopify_schema_name, shopify_table_name) %}
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
-WITH flows AS 
-    (SELECT email_id, flow_name 
-    FROM {{ ref('klaviyo_flows') }}
-    ),
+WITH 
+    {%- for date_granularity in date_granularity_list %}
 
-    all_flow_messages_staging AS 
-    (SELECT *,
-        {{ get_date_parts('email_date') }}
-    FROM 
-        (SELECT 
-            email_date, 
-            email_id,
-            message_id,
-            email_name, 
-            email_subject,
-            person_id, 
-            MAX(unique_received) as received,
-            MAX(unique_opened) as opened,
-            MAX(unique_clicked) as clicked,
-            MAX(unique_unsubscribed) as unsubscribed,
-            COALESCE(SUM(orders),0) as orders,
-            COALESCE(SUM(revenue),0) as revenue
-            {%- if shopify_table_exists %}
-            , COALESCE(SUM(first_orders),0) as first_orders
-            , COALESCE(SUM(repeat_orders),0) as repeat_orders
-            , COALESCE(SUM(first_order_revenue),0) as first_order_revenue
-            , COALESCE(SUM(repeat_order_revenue),0) as repeat_order_revenue
-            {%- endif %}
-        FROM {{ ref('klaviyo_actions')}}
-        WHERE email_type = 'Flow'
-        GROUP BY email_date, email_id, message_id, email_name, email_subject, person_id 
-        )
-    ),
-
-    all_flows_staging AS 
-    (SELECT *, 
-        {{ get_date_parts('email_date') }}
-    FROM 
-        (SELECT 
-            email_date, 
-            email_id,
-            person_id, 
-            MAX(unique_received) as received,
-            MAX(unique_opened) as opened,
-            MAX(unique_clicked) as clicked,
-            MAX(unique_unsubscribed) as unsubscribed,
-            COALESCE(SUM(orders),0) as orders,
-            COALESCE(SUM(revenue),0) as revenue
-            {%- if shopify_table_exists %}
-            , COALESCE(SUM(first_orders),0) as first_orders
-            , COALESCE(SUM(repeat_orders),0) as repeat_orders
-            , COALESCE(SUM(first_order_revenue),0) as first_order_revenue
-            , COALESCE(SUM(repeat_order_revenue),0) as repeat_order_revenue
-            {%- endif %}
-        FROM {{ ref('klaviyo_actions')}}
-        WHERE email_type = 'Flow'
-        GROUP BY email_date, email_id, email_name, email_subject, person_id 
-        )
-    ),
-
-    {% for date_granularity in date_granularity_list %}
-    all_flow_messages_{{date_granularity}} AS 
+    performance_{{date_granularity}} AS 
     (SELECT 
+        date_trunc('{{date_granularity}}',date) as date,
         '{{date_granularity}}' as date_granularity,
-        {{date_granularity}} as date, 
         flow_name,
-        email_name, 
-        email_subject,
-        SUM(received) as received,
-        SUM(opened) as opened,
-        COALESCE(SUM(opened)::decimal/NULLIF(SUM(received),0),0) as open_rate,
-        SUM(clicked) as clicked, 
-        COALESCE(SUM(clicked)::decimal/NULLIF(SUM(received),0),0) as click_rate,
-        SUM(unsubscribed) as unsubscribed, 
-        COALESCE(SUM(unsubscribed)::decimal/NULLIF(SUM(received),0),0) as unsubscribe_rate,
-        COALESCE(SUM(orders),0) as orders,
-        COALESCE(SUM(orders)::decimal/NULLIF(SUM(received),0),0) as order_rate,
-        COALESCE(SUM(revenue),0) as revenue
+        date::varchar||date_granularity||flow_name as unique_key,
+        sum(coalesce(total_recipients,0)) as total_recipients,
+        sum(coalesce(received,0)) as received,
+        sum(coalesce(opens,0)) as opens,
+        sum(coalesce(clicks,0)) as clicks,
+        sum(coalesce(bounced,0)) as bounced,
+        sum(coalesce(unsubscribed,0)) as unsubscribed,
+        sum(coalesce(subscribed,0)) as subscribed,
+        sum(coalesce(total_orders,0)) as total_orders,
+        sum(coalesce(total_revenue,0)) as total_revenue
         {%- if shopify_table_exists %}
-        , COALESCE(SUM(first_orders),0) as first_orders
-        , COALESCE(SUM(repeat_orders),0) as repeat_orders
-        , COALESCE(SUM(first_order_revenue),0) as first_order_revenue
-        , COALESCE(SUM(repeat_order_revenue),0) as repeat_order_revenue
+        , sum(coalesce(first_orders,0)) as first_orders
+        , sum(coalesce(repeat_orders,0)) as repeat_orders
+        , sum(coalesce(first_orders_revenue,0)) as first_orders_revenue
+        , sum(coalesce(repeat_orders_revenue,0)) as repeat_orders_revenue
         {%- endif %}
-    FROM all_flow_messages_staging 
-    LEFT JOIN flows USING(email_id)
-    WHERE flow_name is not null
-    GROUP BY date_granularity, date, flow_name, email_name, email_subject
-    ),
-
-    all_flows_{{date_granularity}} AS 
-    (SELECT 
-        '{{date_granularity}}' as date_granularity,
-        {{date_granularity}} as date, 
-        flow_name,
-        'Messages Combined' as email_name, 
-        'Messages Combined' as email_subject,
-        SUM(received) as received,
-        SUM(opened) as opened,
-        COALESCE(SUM(opened)::decimal/NULLIF(SUM(received),0),0) as open_rate,
-        SUM(clicked) as clicked, 
-        COALESCE(SUM(clicked)::decimal/NULLIF(SUM(received),0),0) as click_rate,
-        SUM(unsubscribed) as unsubscribed, 
-        COALESCE(SUM(unsubscribed)::decimal/NULLIF(SUM(received),0),0) as unsubscribe_rate,
-        COALESCE(SUM(orders),0) as orders,
-        COALESCE(SUM(orders)::decimal/NULLIF(SUM(received),0),0) as order_rate,
-        COALESCE(SUM(revenue),0) as revenue
-        {%- if shopify_table_exists %}
-        , COALESCE(SUM(first_orders),0) as first_orders
-        , COALESCE(SUM(repeat_orders),0) as repeat_orders
-        , COALESCE(SUM(first_order_revenue),0) as first_order_revenue
-        , COALESCE(SUM(repeat_order_revenue),0) as repeat_order_revenue
-        {%- endif %}
-    FROM all_flows_staging 
-    LEFT JOIN flows USING(email_id)
-    WHERE flow_name is not null
-    GROUP BY date_granularity, date, flow_name
-    )
-    {%- if not loop.last %},{%- endif %}
-    {%- endfor %}
-
-SELECT *, 
-    date_granularity||'_'||date||'_'||flow_name||'_'||email_name||'_'||email_subject as unique_key
-FROM    
-    (
-    {% for date_granularity in date_granularity_list -%}  
-    SELECT * 
-    FROM all_flow_messages_{{date_granularity}}
-    UNION ALL 
-    SELECT * 
-    FROM all_flows_{{date_granularity}}
-    {% if not loop.last %}UNION ALL
+    FROM {{ ref('klaviyo_campaigns') }}
+    LEFT JOIN {{ ref('klaviyo_placed_order') }} USING(date,campaign_id)
+    where flow_name != ''
+    GROUP BY 
+        date_granularity,
+        date,
+        flow_name
+        unique_key
+    ){% if not loop.last %},
     {% endif %}
     {%- endfor %}
-    )
+
+    ({% for date_granularity in date_granularity_list -%}
+    SELECT *
+    FROM performance_{{date_granularity}}
+    {% if not loop.last %}UNION ALL
+    {% endif %}
+
+    {%- endfor %}
